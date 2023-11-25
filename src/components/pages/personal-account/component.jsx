fc38b32220af559db0145506d225673aad490ab5
DaniyaRRa <d_raiymbekov@kbtu.kz>
2023-11-25 21:27:51 +0600

    Подтягивание данных юзера из БД в страницу profile

--- src/components/pages/personal-account/component.jsx
+++ src/components/pages/personal-account/component.jsx
@@ -1,26 +1,61 @@
 import classes from "./PersonalAccount.module.scss";
-
+import React, { useEffect, useState } from 'react';
 export const PersonalAccount = () => {
+    const [profile_data, setProfile_data] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const accessToken = localStorage.getItem("access_token");
+
+                const response = await fetch('http://localhost:8000/profile', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+
+                const data = await response.json();
+                setProfile_data(data);
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <section className="d-flex">
-            <div className={classes.photoWrapper}>
-                <img src={require("../../../assets/images/profile.png")} alt="profile" width={150} height={150} />
-                <h3 className={classes.fullname}>Фамилия Имя Отчество</h3>
-            </div>
-            <div className={classes.infoWrapper}>
-                <h3 className={classes.name}>Фамилия Имя</h3>
-                <ul className={classes.infoList}>
-                    <li className={classes.infoDetail}><span>ИИН:</span> 123456789</li>
-                    <li className={classes.infoDetail}><span>Дата рождения:</span> 12.12.1212</li>
-                    <li className={classes.infoDetail}><span>Место рождения:</span> Шымкент</li>
-                    <li className={classes.infoDetail}><span>Национальность:</span> Казах</li>
-                    <li className={classes.infoDetail}><span>Пол:</span> Мужчина</li>
-                    <li className={classes.infoDetail}><span>E-mail:</span> muzhik@mail.ru</li>
-                    <li className={classes.infoDetail}><span>Телефон:</span> +7 777 7777 7777</li>
-                    <li className={classes.infoDetail}><span>Образование:</span> школьное</li>
-                    <li className={classes.infoDetail}><span>Фактический адрес:</span> Алматы</li>
-                </ul>
-            </div>
+            {profile_data ? (
+                <div>
+                    <div className={classes.photoWrapper}>
+                        <img src={require("../../../assets/images/profile.png")} alt="profile" width={150} height={150} />
+                        <h3 className={classes.fullname}>{profile_data.fio}</h3>
+                    </div>
+                    <div className={classes.infoWrapper}>
+                        <h3 className={classes.name}> {profile_data.fio}</h3>
+                        <ul className={classes.infoList}>
+                            <li className={classes.infoDetail}><span>ИИН:</span> {profile_data.iin_bin}</li>
+                            <li className={classes.infoDetail}><span>Дата рождения:</span> {profile_data.birth_date}</li>
+                            <li className={classes.infoDetail}><span>Место рождения:</span> {profile_data.birth_place}</li>
+                            <li className={classes.infoDetail}><span>Национальность:</span> {profile_data.nationality}</li>
+                            <li className={classes.infoDetail}><span>Пол:</span> {profile_data.sex}</li>
+                            <li className={classes.infoDetail}><span>E-mail:</span> {profile_data.email}</li>
+                            <li className={classes.infoDetail}><span>Телефон:</span> {profile_data.phone_number}</li>
+                            <li className={classes.infoDetail}><span>Образование:</span> {profile_data.education}</li>
+                            <li className={classes.infoDetail}><span>Фактический адрес:</span> {profile_data.current_address}</li>
+                        </ul>
+                    </div>
+                </div>
+            ) : (
+                <p></p>
+            )}
         </section>
     )
 }
