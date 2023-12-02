0d6807fe1adab6c7ee2226fa03c591466ecd788a
Ualikhan Maxat <walikhan19112002@gmail.com>
2023-12-02 14:28:51 +0600

    Added nation-birthplace

--- src/api/controllers/auth.js
+++ src/api/controllers/auth.js
@@ -1,9 +1,9 @@
 import { postAsync } from "../functions";
 
-const ROOT = "http://127.0.0.1:8000"; 
+const ROOT = "http://127.0.0.1:8000";
 
 export const login = async (iinbin, password) =>
   postAsync(`${ROOT}/login`, JSON.stringify({ iinbin, password }));
 
-export const registration = async ( iinbin, fullName, password, birthdate) =>
-  postAsync(`${ROOT}/register`, JSON.stringify({ iinbin, password, fullName, birthdate }));
+export const registration = async (iinbin, fullName, password, birthplace, nation) =>
+  postAsync(`${ROOT}/register`, JSON.stringify({ iinbin, password, fullName, birthplace, nation }));
